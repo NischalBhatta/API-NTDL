df9d4aebcbc2b459f85c3f270ab8c983cb7f8619
NischalBhatta <nischalpb@gmail.com>
2026-08-07 16:19:23 +1000

    Created API routers for Not to do List. Need to integrate with frontend and database.

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+const PORT = 8000;
+const app = express();
+import morgan from "morgan";
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+import taskRouters from "./src/routers/taskRouters.js";
+
+app.use("/api/v1/tasks", taskRouters);
+
+app.use("/", (req, res) => {
+  res.json({
+    status: "Success",
+    message: "TODO",
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`Server is running at http://localhost:${PORT}`);
+});
